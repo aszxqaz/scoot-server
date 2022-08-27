2824b3b88a9f01266ec22572f4df5bcaf80ef9f4
aszxqaz <maxim.butenko92.1@gmail.com>
2022-08-28 02:42:44 +0300

    stable

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+const path = require('path');
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('')
+  getHello(@Res() res: Response) {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+  }
+}
